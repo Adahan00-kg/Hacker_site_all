1c615df3d6fd0913812efe4b6fc80b1bbda214aa
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2025-02-07 11:15:08 +0600

    not all models Laptop

--- mysite/laptop/models.py
+++ mysite/laptop/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100,unique=True)
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=150,unique=True)
+
+
+class Laptop(models.Model):
+    laptop_name = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='laptop')
+    brand = models.ForeignKey(Brand,on_delete=models.CASCADE,related_name='brand')
+    laptop_discount = models.PositiveSmallIntegerField(null=True,blank=True,help_text='процент скидки')
+
+
+
+class PhotoLaptop(models.Model):
+    img = models.FileField(upload_to='laptop_photo/')
+    color = models.CharField(max_length=150)
+    laptop_connect = models.ForeignKey(Laptop,on_delete=models.CASCADE,related_name='color')
+
+
+
